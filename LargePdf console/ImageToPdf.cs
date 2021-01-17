d63f0e66100a11582fd20377d7a523add26b8114
Alluri Vamsi <vamsi1306@gmail.com>
2021-01-18 05:06:35 +0530

    Implemented image to pdf conversion.

---
+++ LargePdf console/ImageToPdf.cs
@@ -0,0 +1,68 @@
+﻿using Aspose.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LargePdf_console
+{
+    public class ImageToPdf
+    {
+        private const string ImagesToPdf = "Images to PDF";
+        private static readonly string DefaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public ImageToPdf() { }
+
+        public void ConvertImageToPdf()
+        {
+            Console.WriteLine(ImagesToPdf);
+            Console.WriteLine("FolderSelect:");
+
+            var filesInPath = FolderSelect();
+            if (filesInPath == null)
+            {
+                Console.WriteLine("No files found in the given directory.");
+                return;
+            }
+            Document doc = new Document();
+            Page page = doc.Pages.Add();
+
+
+            Console.WriteLine();
+            foreach (var file in filesInPath)
+            {
+                Image image = new Image
+                {
+                    File = file
+                };
+                page.PageInfo = Pages.A4;
+                page.Paragraphs.Add(image);
+            }
+            var DirectoryName = Path.GetDirectoryName(filesInPath.FirstOrDefault());
+            var FileName = Path.GetFileName(DirectoryName);
+            doc.Save($"{DirectoryName}\\{FileName}.pdf"); 
+            Process.Start("explorer.exe", DirectoryName);
+        }
+
+        private IEnumerable<string> FolderSelect()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog
+            {
+                SelectedPath = Program.SelectedDirectory ?? DefaultDirectory
+            };
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                List<string> filePaths = new List<string>();
+                foreach (var path in Directory.GetFiles(fbd.SelectedPath))
+                {
+                    filePaths.Add(path);
+                }
+                Program.SelectedDirectory = fbd.SelectedPath;
+                return filePaths;
+            }
+            return null;
+        }
+    }
+}
---
+++ LargePdf console/Pages.cs
@@ -0,0 +1,24 @@
+﻿using Aspose.Pdf;
+
+namespace LargePdf_console
+{
+    public class Pages
+    {
+        #region A4
+        public static PageInfo A4 = CreateA4();
+        static PageInfo CreateA4()
+        {
+            var tempo = new PageInfo
+            {
+                Height = 7016,
+                Width = 4960,
+                Margin = NoMargin()
+
+            };
+            MarginInfo NoMargin() => new MarginInfo(50, 50, 50, 50);
+
+            return tempo;
+        }
+        #endregion
+    }
+}
