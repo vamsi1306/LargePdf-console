a920639f371430d4c60ed9404efe8c3cef3e2481
Alluri Vamsi <vamsi1306@gmail.com>
2021-01-18 05:10:57 +0530

    Implemented image to pdf conversion. (#7)

--- LargePdf console/Program.cs
+++ LargePdf console/Program.cs
@@ -1,43 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LargePdf_console
 {
-    class Program
+    public class Program
     {
-        private const string ImagesToPdf = "Images to PDF";
-        private static string DefaultDirectory = "D:\\Users\\vamsi\\Documents\\My Files\\Classic Sapphire\\Building Permit Order"; //TODO: Need to decide on a directory name, no need to enter the path every time. 
-        private const string EnterTheNewDirectory = "Enter the new directory: ";
+        public static string SelectedDirectory;
 
-        // Also, should give an option to change the default directory. Use a json file for storing the data/ settings.
-
-
-        static void Main(string[] args)
+        [STAThread]
+        public static void Main(string[] args)
         {
-            Console.WriteLine(ImagesToPdf);
-            Console.WriteLine($"The default path is {DefaultDirectory}. Want to change the default one: (Y)");
-            var ChangeDirectory = Console.ReadKey().KeyChar;
-            if (Equals(ChangeDirectory, 'Y') || Equals(ChangeDirectory, 'y'))
+            do
             {
-                Console.WriteLine(EnterTheNewDirectory);
-                DefaultDirectory = Console.ReadLine();
-            }
-            //var directory = System.IO.Directory.GetCurrentDirectory();
-            var filesInDirectory = GetFiles(DefaultDirectory);
-            Console.WriteLine();
-
-            foreach (var file in filesInDirectory)
-                Console.WriteLine($"{file}");
-            Console.WriteLine();
-
-            Console.WriteLine("Press anything to exit");
-            Console.ReadKey();
+                ImageToPdf converter = new ImageToPdf();
+                converter.ConvertImageToPdf();
+            } while (ShouldContinue());
         }
 
-        private static string[] GetFiles(string directory) => Directory.GetFiles(directory);
+        private static bool ShouldContinue()
+        {
+            Console.Clear();
+            Console.WriteLine("\nWant to convert another pdf. (Y)");
+            return IsYes(Console.ReadKey().KeyChar);
+        }
+
+        private static bool IsYes(char value) => value == 'Y' || value == 'y';
+
     }
 }
