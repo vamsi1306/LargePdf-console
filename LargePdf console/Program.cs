30273fa0e7503e2f1c8f31f969b1896ce775dd22
Alluri Vamsi <vamsi1306@gmail.com>
2021-01-18 02:04:20 +0530

    Implemented Getting file names. Need a lot of cleaning.

--- LargePdf console/Program.cs
+++ LargePdf console/Program.cs
@@ -10,8 +10,8 @@ namespace LargePdf_console
     class Program
     {
         private const string ImagesToPdf = "Images to PDF";
-        private const string DefaultDirectory = ""; //TODO: Need to decide on a directory name, no need to enter the path every time. 
-        private static string EnterTheNewDirectory = "Enter the new directory: ";
+        private static string DefaultDirectory = "D:\\Users\\vamsi\\Documents\\My Files\\Classic Sapphire\\Building Permit Order"; //TODO: Need to decide on a directory name, no need to enter the path every time. 
+        private const string EnterTheNewDirectory = "Enter the new directory: ";
 
         // Also, should give an option to change the default directory. Use a json file for storing the data/ settings.
 
@@ -24,10 +24,20 @@ namespace LargePdf_console
             if (Equals(ChangeDirectory, 'Y') || Equals(ChangeDirectory, 'y'))
             {
                 Console.WriteLine(EnterTheNewDirectory);
-                EnterTheNewDirectory = Console.ReadLine();
+                DefaultDirectory = Console.ReadLine();
             }
+            //var directory = System.IO.Directory.GetCurrentDirectory();
+            var filesInDirectory = GetFiles(DefaultDirectory);
+            Console.WriteLine();
 
-            
+            foreach (var file in filesInDirectory)
+                Console.WriteLine($"{file}");
+            Console.WriteLine();
+
+            Console.WriteLine("Press anything to exit");
+            Console.ReadKey();
         }
+
+        private static string[] GetFiles(string directory) => Directory.GetFiles(directory);
     }
 }
